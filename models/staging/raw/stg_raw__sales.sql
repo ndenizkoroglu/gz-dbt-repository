c2f409ea44fadae7385a32c65923e5a58e2d55ed
dkoroglu088@gmail.com <dkoroglu088@gmail.com>
2025-10-23 23:40:18 +0000

    Clean & rename in stg_raw__sales

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -1,7 +1,11 @@
+-- models/staging/raw/stg_raw__sales.sql
+
 with source as (
   select * from {{ source('raw', 'sales') }}
 ),
-renamed as (
+
+-- NULL kayıtları ele
+clean as (
   select
     date_date,
     orders_id,
@@ -9,8 +13,23 @@ renamed as (
     revenue,
     quantity
   from source
+  where orders_id is not null
+    and pdt_id  is not null
 )
-select * from renamed
+
+-- (İsteğe bağlı) duplicate varsa en güncel kaydı tutmak için aç:
+-- ,dedup as (
+--   select *
+--   from clean
+--   qualify row_number() over (
+--     partition by orders_id, products_id
+--     order by date_date desc
+--   ) = 1
+-- )
+
+select * from clean;
+-- duplicate temizleme istiyorsan: select * from dedup;
+
 
 
 
