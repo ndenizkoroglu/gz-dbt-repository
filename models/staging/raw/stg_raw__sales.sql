7144da3af61e7d5addc152dcaf95b3a9c49be302
dkoroglu088@gmail.com <dkoroglu088@gmail.com>
2025-10-23 23:12:54 +0000

    Add stg_raw__sales model

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -1,4 +1,18 @@
-dbt build --select stg_raw__sales
+with source as (
+  select * from {{ source('raw', 'sales') }}
+),
+renamed as (
+  select
+    date_date,
+    orders_id,
+    pdt_id as products_id,
+    revenue,
+    quantity
+  from source
+)
+select * from renamed
+
+
 
 
 
